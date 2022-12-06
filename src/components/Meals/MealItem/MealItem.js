6b41281cf0d6ebf6f56b75f95832fb30a4578472
imakhlaq <imakhlaqXD@gmail.com>
2022-12-06 15:04:50 +0530

    meals

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+
+const MeaslItem = (props) => {
+  const price = `$ ${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3 className={classes.name}>{props.name}</h3>
+        <div className={classes.discription}>{props.discription}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
+      </div>
+    </li>
+  );
+};
+export default MeaslItem;
