dda0188ad86f32a24be6f39270ebe7f7cb5727e9
imakhlaq <imakhlaqXD@gmail.com>
2022-12-08 14:33:51 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import { useState } from "react";
 
 function App() {
+  const [cartState, setCartState] = useState(false);
+
+  const closeCart = () => {
+    setCartState(false);
+  };
+  const openCart = () => {
+    setCartState(true);
+  };
   return (
     <>
       <Header />
       <main>
-        <Cart />
+        {cartState && <Cart />}
         <Meals />
       </main>
     </>
